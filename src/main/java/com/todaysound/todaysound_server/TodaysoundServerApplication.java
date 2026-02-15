e39917ebce1301fdf9a335790aecaa65f3472b91
박지현 <102128060+wlgusqkr@users.noreply.github.com>
2026-02-15 22:12:19 +0900

    e

--- src/main/java/com/todaysound/todaysound_server/TodaysoundServerApplication.java
+++ src/main/java/com/todaysound/todaysound_server/TodaysoundServerApplication.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
+@EnableScheduling // 스케줄링 활성화
 public class TodaysoundServerApplication {
 
     public static void main(String[] args) {
